cda32a89cae21815a37c3532b5286bbd9f64078a
azuraopal <muhammadnovals334@gmail.com>
2026-02-09 08:44:09 +0700

    (Refactor): Fix namespace import for PengembalianResource in CreatePengembalian page

--- app/Filament/Petugas/Resources/Pengembalian/Pages/CreatePengembalian.php
+++ app/Filament/Petugas/Resources/Pengembalian/Pages/CreatePengembalian.php
@@ -2,7 +2,7 @@
 
 namespace App\Filament\Petugas\Resources\Pengembalian\Pages;
 
-use App\Filament\Petugas\Resources\Pengembalians\PengembalianResource;
+use App\Filament\Petugas\Resources\Pengembalian\PengembalianResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreatePengembalian extends CreateRecord
